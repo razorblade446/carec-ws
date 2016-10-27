661f4ece879a90aab55d4c4a1568e9e44c830ed5
Frederic Yesid Peña Sánchez <fredericpena@hotmail.com>
2016-10-26 20:10:47 -0500

    * test for carec

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const PORT = process.env.PORT || 3232;
 
-app.set('port', (process.env.PORT || 5000));
+app.use(cors());
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.use(express.static('public'));
+app.post('/chat', (req, res) => {
+    res.redirect(`/chat`);
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+require('./Signaling-Server.js')(server, function(socket) {
+    try {
+        var params = socket.handshake.query;
+
+        // "socket" object is totally in your own hands!
+        // do whatever you want!
+
+        // in your HTML page, you can access socket as following:
+        // connection.socketCustomEvent = 'custom-message';
+        // var socket = connection.getSocket();
+        // socket.emit(connection.socketCustomEvent, { test: true });
+
+        if (!params.socketCustomEvent) {
+            params.socketCustomEvent = 'custom-message';
+        }
+
+        socket.on(params.socketCustomEvent, function(message) {
+            try {
+                socket.broadcast.emit(params.socketCustomEvent, message);
+            } catch (e) {
+                console.log(e);
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
 });
 
-
+server.listen(PORT, () => console.log(`chat-snap is listening on port ${PORT}`));
